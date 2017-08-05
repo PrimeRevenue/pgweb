3fe81ae498f77fac351c0f8f0ff9a19bbdd0dad9
John Lawrie <jlawrie@primerevenue.com>
2017-08-04 23:45:09 -0400

    adding lock session

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.pgweb/bookmarks
 cat > ~/.pgweb/bookmarks/server.toml << EOF
 url = "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB?sslmode=disable"
 EOF
-executable="/usr/bin/pgweb --bind=0.0.0.0 --listen=8081 --bookmark=server"
+executable="/usr/bin/pgweb --bind=0.0.0.0 --listen=8081 --lock-session --bookmark=server"
 if [[ $DEBUG ]]; then
     if $DEBUG; then
     executable="$executable --debug"
@@ -15,6 +15,5 @@ fi
 if $READONLY; then
        executable="$executable --readonly"
 fi
-
 echo "$executable"
 $executable
