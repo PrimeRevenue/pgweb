9df5b6aeedbc5910849ca7213210dd32c300f98d
Chris Griffin <cgriffin@primerevenue.com>
2018-05-30 14:04:51 -0400

    Update entrypoint.sh
    Needed to update to support connecting to paytrack over ssl.  I have already built, tested and pushed: primerevenue/pgweb:0.9.11

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 . /env_secrets_expand.sh
 mkdir -p ~/.pgweb/bookmarks
 cat > ~/.pgweb/bookmarks/server.toml << EOF
-url = "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB?sslmode=disable"
+url = "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB?sslmode=$POSTGRES_SSLMODE"
 EOF
 executable="/usr/bin/pgweb --bind=0.0.0.0 --listen=8081 --lock-session --bookmark=server"
 if [[ $DEBUG ]]; then
